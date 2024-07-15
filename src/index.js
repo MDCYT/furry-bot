f1faf4a253ae98991a5912b0e9a23188cfb4850f
José Ortiz <me@mdcdev.me>
2024-07-15 16:52:52 -0500

    :3

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
 import process from 'node:process';
 import { URL } from 'node:url';
 import { Client, GatewayIntentBits } from 'discord.js';
+import express from 'express';
 import { loadCommands, loadEvents } from './util/loaders.js';
 import { registerEvents } from './util/registerEvents.js';
 
+const PORT = process.env.PORT || 3_000;
+
 // Initialize the client
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.DirectMessages] });
+
+// Initialize the express server
+const app = express();
+
+// Set up the server
+app.get('/', (req, res) => {
+	// Show the ip address of the user
+	const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+	res.send(
+		`<body style="background-color: #36393f; color: #fff; font-family: Arial, sans-serif; text-align: center; padding-top: 20%;">Your IP address is: ${ip} :3</body>`,
+	);
+});
 
 // Load the events and commands
 const events = await loadEvents(new URL('events/', import.meta.url));
@@ -16,3 +31,8 @@ registerEvents(commands, events, client);
 
 // Login to the client
 void client.login(process.env.DISCORD_TOKEN);
+
+// Start the server
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`);
+});
